d4d65be170131fae90437e732cc1cf1144d0eca9
Yonanni <adhanamengisteab@gmail.com>
2022-03-05 21:35:34 +0100

    user middleware

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,7 @@
 import pkg from "@prisma/client";
 import bcrypt from "bcrypt";
+import createError from "http-errors";
+import { verifyToken } from "./tokenVerify.js";
 
 const {PrismaClient} = pkg
 const prisma = new PrismaClient()
@@ -21,4 +23,33 @@ export const checkCredential = async function (email, plainPassword) {
       return null;
     }
   };
+
+  export const userAuthMiddleware = async(req, res, next) => {
+    try {
+      if (!req.headers.authorization){
+        console.log("bearer", req.headers.authorization)
+        next(createError(401, "Login first please!"))
+      } else {
+        const token = req.headers.authorization.replace("Bearer ", "")
+        const decode = await verifyToken(token)
+        // console.log("token&decode--", token, decode)
+        const user = await prisma.user.findFirst({
+          where: {
+            id: decode.id
+          }
+        })
+        if (user){
+          req.user = user
+          next()
+        } else {
+          next(createError(401, "Unauthorized!!!"))
+          console.log("bearer--------")
+        }
+      }
+    } catch (error) {
+      console.log(error)
+      next(createError(401, "something went wrong"))
+      console.log("bearer-------->>>")
+    }
+  }
   export default prisma
